1a4c7a7c33a1c421a85bd318ac6bff0095072965
root <root@Kuro>
2023-10-11 19:35:47 -0400

    Refactor JWT Authentication Middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,14 +3,18 @@ const jwt = require('jsonwebtoken');
 function authenticateJWT(req, res, next) {
     const authHeader = req.headers.authorization;
 
+    // Check if the Authorization header exists
     if (authHeader) {
-        const token = authHeader.split(' ')[1];  // Extract the token from the Authorization header
+        // Extract the token from the Authorization header
+        const token = authHeader.split(' ')[1];
 
+        // Verify the token
         jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
             if (err) {
                 return res.status(403).json({ message: 'Invalid token' });
             }
 
+            // Attach the user payload to the request object
             req.user = user;
             next();
         });
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
