fa86effa813c704f0a795a492453c063a1eb01ca
root <root@Kuro>
2023-10-11 13:00:50 -0400

    added user login route with JWT authentication as well as error handling

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -36,6 +37,38 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// User login route
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check if email and password are provided
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Please provide email and password.' });
+        }
+
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password.' });
+        }
+
+        // Compare provided password with stored hashed password
+        const isMatch = await bcrypt.compare(password, user.hashedPassword);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid email or password.' });
+        }
+
+        // Generate JWT
+        const token = jwt.sign({ id: user._id }, 'YOUR_SECRET_KEY', { expiresIn: '1h' }); // Replace 'YOUR_SECRET_KEY' with a strong secret key
+
+        res.status(200).json({ message: 'Logged in successfully!', token });
+
+    } catch (error) {
+        console.error('Error logging in user:', error);
+        res.status(500).json({ message: 'Server error. Please try again later.' });
+    }
+});
 // TODO: Add other user-related routes (e.g., login, profile management, etc.)
 
 module.exports = router;
